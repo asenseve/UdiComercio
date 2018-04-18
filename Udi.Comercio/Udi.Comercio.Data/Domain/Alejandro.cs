df1af57c748a6bfce6638a3abcea8242deb59ffa
alejandro.senseve <asenseve@gmail.com>
2018-04-17 20:24:31 -0400

    Subiendo mi clase

---
+++ Udi.Comercio/Udi.Comercio.Data/Domain/Alejandro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udi.Comercio.Data.Domain
+{
+    class Alejandro
+    {
+        public Alejandro()
+        {
+             // Hola Mundo
+        }
+
+    }
+}
