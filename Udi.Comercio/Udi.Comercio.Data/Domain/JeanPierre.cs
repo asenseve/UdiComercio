95f5bed29bb6412080e751e6565b4e25a9a13a68
Jean Pierre Velez <vcjpierre@gmail.com>
2018-04-24 20:04:42 -0400

    Añadiendo cambios de clase Jean Pierre

--- Udi.Comercio/Udi.Comercio.Data/Domain/JeanPierre.cs
+++ Udi.Comercio/Udi.Comercio.Data/Domain/JeanPierre.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// 24-04-2018
+// Nombre: Jean Pierre Velez
+// Fec.Nac: 01/27/1997
+// CI. 13172917SC
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +14,10 @@ namespace Udi.Comercio.Data.Domain
     public class JeanPierre
     {
         public string MiNombre { get; set; }
+
+        public string Mensaje(string msj)
+        {
+            return msj = "Hola Mundo";
+        }
     }
 }
